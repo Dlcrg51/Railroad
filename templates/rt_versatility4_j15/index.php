12f7d028d1038b8f3aaf9ba504311f61cc8e8abf
dave@cohesivewebsites.com <useruser@Davess-iMac.local>
2014-02-03 13:54:18 -0500

    added bootstrap call, edited colors

--- templates/rt_versatility4_j15/index.php
+++ templates/rt_versatility4_j15/index.php
@@ -86,6 +86,12 @@ $ms_module10				= $this->params->get("msModule10", "user20");
 								
 	require(YOURBASEPATH . "/rt_styleloader.php");
 	
+	// Add JavaScript Frameworks
+	JHtml::_('bootstrap.framework');
+	
+	// Load optional RTL Bootstrap CSS
+	JHtml::_('bootstrap.loadCss');
+	
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
